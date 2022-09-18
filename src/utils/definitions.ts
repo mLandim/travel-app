db1577d68f71e8d71bf6e7b08c78a40b75f164e8
Marcelo Landim <marcelolandim85@gmail.com>
2022-09-18 10:18:52 -0300

    update - dry for interfaces

---
+++ src/utils/definitions.ts
@@ -0,0 +1,26 @@
+
+export interface Coordinates {
+    lat: string
+    lng: string
+}
+
+export interface GeoName {
+    adminCode1: string
+    adminCodes1: {ISO3166_2: string}
+    adminName1: string
+    countryCode: string
+    countryId: string
+    countryName: string
+    distance: string
+    fcl: string
+    fclName: string
+    fcode: string
+    fcodeName: string
+    geonameId: number
+    lat: string
+    lng: string
+    name: string
+    population: number
+    toponymName: string
+  }
+  
